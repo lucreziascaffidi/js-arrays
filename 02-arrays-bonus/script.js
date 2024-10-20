a5ccb531fe7350f9db6c204e1533066991359ed2
lucreziascaffidi <lucrezia.scaffidi@gmail.com>
2024-10-20 18:23:06 +0200

    feat: starte dand finished project

---
+++ 02-arrays-bonus/script.js
@@ -0,0 +1,23 @@
+const teachers = [
+  'Nathan',
+  'Ed',
+  'Fabio',
+  'Phil',
+  'Carlo',
+  'Lewis',
+  'Luca'
+]; // NON MODIFICARE QUESTA VARIABILE
+
+// 1. Inverti l'ordine degli insegnanti nell'array teachers
+// e salva il risultato nella variabile reversedTeachers
+const reversedTeachers = teachers.reverse();
+console.log(reversedTeachers);
+
+// 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
+// con un nome di lunghezza maggiore o uguale a 5 caratteri
+const longNames = teachers.filter((teachersName) => teachersName.length >= 5);
+console.log(longNames);
+
+// 3. Rimuovi 'Ed' dall'array teachers
+teachers.splice(5, 1);
+console.log(teachers);
